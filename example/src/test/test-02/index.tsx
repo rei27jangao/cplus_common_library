ea0844b3a2434c7d1850d582aeea0722eba58f45
rei27jangao <63288662+rei27jangao@users.noreply.github.com>
2020-06-01 17:05:30 +0800

    fix formIssue

--- example/src/test/test-02/index.tsx
+++ example/src/test/test-02/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FormComponent, CountryInput, SubmitButton, NumberInput } from 'cplus_common_library'
+import { CountryInput, NumberInput } from 'cplus_common_library'
 
 const TestComponent02: React.FC = () => {
   return (
@@ -12,41 +12,36 @@ const TestComponent02: React.FC = () => {
       >
         Testing Components2
       </h1>
-      <FormComponent
-        propValues={{inputForm: ''}}
-        onSubmit={(values: any) => {
-          window.alert(JSON.stringify(values))
-        }}>
-        <CountryInput
-          isRequired={true}
-          // value="JP"
-          texts={{
-            empty: 'Please fill the required field',
-            invalid: 'Invalid country code format.' }}
-          attrs={{
-            name: 'inputForm', 
-            title: 'Input Title', 
-            placeholder: 'Enter a Country code', 
-            style: { width: '10%' }
-          }}
-        />
-        <NumberInput
-          isRequired={true}
-          isDecimal={true}
-          texts={{
-            empty: 'Please fill the required field',
-            invalid: 'Format incorrect'
-          }}
-          attrs={{
-            name: 'number01',
-            title: 'Input Number', 
-            placeholder: 'Enter a text', 
-            min: 1, 
-            max: 10,
-            style: { width: '10%' }
-          }}
-         />
-      </FormComponent>
+      <CountryInput
+        isRequired={true}
+        // value="JP"
+        texts={{
+          empty: 'Please fill the required field',
+          invalid: 'Invalid country code format.'
+        }}
+        attrs={{
+          name: 'inputForm',
+          title: 'Input Title',
+          placeholder: 'Enter a Country code',
+          style: { width: '10%' }
+        }}
+      />
+      <NumberInput
+        isRequired={true}
+        isDecimal={true}
+        texts={{
+          empty: 'Please fill the required field',
+          invalid: 'Format incorrect'
+        }}
+        attrs={{
+          name: 'number01',
+          title: 'Input Number',
+          placeholder: 'Enter a text',
+          min: 1,
+          max: 10,
+          style: { width: '10%' }
+        }}
+      />
     </div>
   )
 }
