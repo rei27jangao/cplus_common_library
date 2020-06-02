add86ab8f53348f35f29dfb800a95a9f1ec15aa3
TCAP_romeo <romeo-jr.estropigan1@trans-cosmos.co.jp>
2020-06-02 16:15:02 +0800

    feat: react-select

--- example/src/App.js
+++ example/src/App.js
@@ -2,14 +2,15 @@ import React from 'react'
 import TestComponent01 from './test/test-01'
 import TestComponent02 from './test/test-02'
 import TestComponent03 from './test/test-03'
+import './style.css'
 
 const App = () => {
   return(
-    <>
+    <div className="containerFlex">
       <TestComponent01/>
       <TestComponent02/>
       <TestComponent03/>
-    </>
+    </div>
   )
 }
 
--- example/src/test/test-03/index.tsx
+++ example/src/test/test-03/index.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { TextAreaInput } from 'cplus_common_library'
+import { TextAreaInput, SelectInput } from 'cplus_common_library'
+
+export const dogOptions = [
+  { id: 1, label: 'Chihuahua' },
+];
 
 const TestComponent03: React.FC = () => {
   return (
@@ -15,13 +19,22 @@ const TestComponent03: React.FC = () => {
         Testing Components 3
       </h1>
       <TextAreaInput 
-        name="Description" 
-        placeholder="Input Description" 
-        minLength={3} /* not required */
-        maxLength={10} /* not required */
+        value=''
+        name='Description'
+        required={true}
+        maxLength={20}
+        errors={{
+          empty: 'Please fill out this field',
+          invalid: 'Format incorrect'
+        }}
+        className='nothing'
+        attrs={{
+          placeholder: 'Enter description'
+        }}
       />
+      <SelectInput /* options={dogOptions} */ />
     </div>
   )
 }
-
+  
 export default TestComponent03
