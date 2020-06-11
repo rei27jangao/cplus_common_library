5a9a9f6d0b152a503d680caed1f16cdbc91f7189
TCAP Sarah <sarah.baybay1@trans-cosmos.co.jp>
2020-06-11 16:07:15 +0800

    fixed country code and number input

--- src/components/number-input/index.tsx
+++ src/components/number-input/index.tsx
@@ -1,25 +1,30 @@
 import React, { useState } from 'react'
 import * as types from './types'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { Label, Input } from 'reactstrap'
-import { RequiredSign } from '../common/required-indication'
+import { Input } from 'reactstrap'
+import { ErrorMessage } from '../common/error-message'
+import { toHalfWidth } from '../../utils/toHalfWidthConverter'
+import { numberFullAndHalfWidth } from '../../utils/regex'
 
-export const NumberInput: React.FC<types.InputType> = ({
+export const NumberInput: React.FC<types.InputNumberType> = ({
   isRequired,
   isDecimal,
-  attrs,
+  innerProps,
   texts,
   value,
   onChange,
   decimalPlace,
-  valid,
+  inputClassName,
+  inputInlineStyle,
   invalid,
-  className,
-  innerRef
+  innerRef,
+  min,
+  max
 }) => {
   const [textVal, setTextVal] = useState(value)
   const [errMessage, setErrMessage] = useState('')
   const decimalCount = decimalPlace ? decimalPlace + 1 : decimalPlace || 3;
+
   const onChangeHandler = (value: any) => {
     onChange(value);
     const num = value.target.value;
@@ -38,16 +43,16 @@ export const NumberInput: React.FC<types.InputType> = ({
         checkValue(num1.substr(0, decimal));
       }
     } else {
-      const check = num.replace(/[^0-9０-９]/g, '')
-      checkValue(check)
-      setTextVal(check)
+      const check = num.replace(numberFullAndHalfWidth, '')
+      checkValue(check);
+      setTextVal(check);
     }
   }
 
   const checkValue = (num: any) => {
     if (num.length !== 0) {
       setErrMessage('')
-      if (num > attrs.max || num < attrs.min) {
+      if (num > max || num < min) {
         setErrMessage(texts.invalid);
       } else {
         setErrMessage('');
@@ -57,46 +62,41 @@ export const NumberInput: React.FC<types.InputType> = ({
     }
   }
 
-  const toASCII = (chars: any) => {
-    var ascii = ''
-    for (var i = 0, l = chars.length; i < l; i++) {
-      var c = chars[i].charCodeAt(0)
-      if (c >= 0xff00 && c <= 0xffef) {
-        c = 0xff & (c + 0x20)
-      }
-      ascii += String.fromCharCode(c)
+  const sliceLast = (value:any) => {
+    if (value.charAt(value.length - 1) === '.') {
+      setTextVal(value.slice(0, -1));
     }
-    const num = parseInt(ascii);
-    if (num > attrs.max || num < attrs.min) {
-      setTextVal('')
+  }
+
+  const convertToHalfWidth = (chars: any) => {
+    const convertedText = toHalfWidth(chars);
+    const num = parseInt(convertedText);
+    if (num > max || num < min) {
       setErrMessage(texts.invalid);
-      checkValue(ascii);
+      checkValue(convertedText);
+      sliceLast(convertedText);
     } else {
-      setTextVal(ascii);
+      sliceLast(convertedText);
       setErrMessage('');
-      checkValue(ascii);
+      checkValue(convertedText);
     }
   }
 
   return (
     <React.Fragment>
-      <Label>
-        {attrs.title} { isRequired && <RequiredSign />}
-      </Label>
       <Input
+        {...innerProps}
         type="text"
         value={textVal}
         required={isRequired}
-        name={attrs.name}
-        placeholder={attrs.placeholder}
-        style={attrs.style}
         onChange={(value: any) => onChangeHandler(value)}
-        onBlur={() => toASCII(textVal)}
-        invalid={errMessage !== ''}
-        className={(valid ? "is-valid " : invalid ? "is-invalid " : "") + className}
+        onBlur={() => convertToHalfWidth(textVal)}
+        invalid={invalid || errMessage !== ''}
+        className={inputClassName}
         innerRef={innerRef}
+        style={inputInlineStyle}
       />
-      <p className='text-danger'>{errMessage}</p>
+      {errMessage !== "" && <ErrorMessage error={errMessage}/>}
     </React.Fragment>
   )
 }
